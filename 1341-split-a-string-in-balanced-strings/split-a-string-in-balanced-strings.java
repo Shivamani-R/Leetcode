553fbe9f229bdb9c7b2551692acb08cd430027c7
Shivamani-R <158829688+Shivamani-R@users.noreply.github.com>
2025-06-08 13:54:06 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (69.87%) - LeetSync

--- 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.java
+++ 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.java
@@ -3,11 +3,11 @@ class Solution {
         int b=0;
         int c=0;
         for(char ch:s.toCharArray()){
-            if(ch=='L'){
-                b--;
-            }
-            else if(ch=='R'){
+            if(ch=='R'){
                 b++;
+            }
+            else if(ch=='L'){
+                b--;
             }
             if(b==0){
                 c++;
