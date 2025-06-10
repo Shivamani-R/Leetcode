2125b6eb5717cacb125a110f7c3cad50e605ed76
Shivamani-R <158829688+Shivamani-R@users.noreply.github.com>
2025-06-10 16:05:35 +0530

    Time: 3 ms (54.69%) | Memory: 43 MB (19.45%) - LeetSync

--- 3753-maximum-difference-between-even-and-odd-frequency-i/maximum-difference-between-even-and-odd-frequency-i.java
+++ 3753-maximum-difference-between-even-and-odd-frequency-i/maximum-difference-between-even-and-odd-frequency-i.java
@@ -1,18 +1,47 @@
+// class Solution {
+//     public int maxDifference(String s) {
+//         int[] mpp = new int[26];
+//         int maxi = 0, mini = s.length();
+//         for (char c : s.toCharArray()) {
+//             mpp[c - 'a']++;
+//         }
+//         for (int i = 0; i < 26; i++) {
+//             if (mpp[i] % 2 != 0) {
+//                 maxi = Math.max(maxi, mpp[i]);
+//             }
+//             if (mpp[i] % 2 == 0 && mpp[i] > 0) {
+//                 mini = Math.min(mini, mpp[i]);
+//             }
+//         }
+//         return maxi - mini;
+//     }
+// }
 class Solution {
+   
     public int maxDifference(String s) {
-        int[] mpp = new int[26];
-        int maxi = 0, mini = s.length();
-        for (char c : s.toCharArray()) {
-            mpp[c - 'a']++;
+       
+        Map<Character, Integer> freq = new HashMap<>();
+        for (char ch : s.toCharArray()) {
+            freq.put(ch, freq.getOrDefault(ch, 0) + 1);
         }
-        for (int i = 0; i < 26; i++) {
-            if (mpp[i] % 2 != 0) {
-                maxi = Math.max(maxi, mpp[i]);
-            }
-            if (mpp[i] % 2 == 0 && mpp[i] > 0) {
-                mini = Math.min(mini, mpp[i]);
+
+        
+        int bestOdd  = Integer.MIN_VALUE; 
+        int bestEven = Integer.MAX_VALUE; 
+
+        for (int count : freq.values()) {
+            if (count % 2 != 0) {            
+                bestOdd = Math.max(bestOdd, count);
+            } else {                           
+                bestEven = Math.min(bestEven, count);
             }
         }
-        return maxi - mini;
+
+        
+        if (bestOdd == Integer.MIN_VALUE || bestEven == Integer.MAX_VALUE) {
+            return -1;
+        }
+
+        return bestOdd - bestEven;  
     }
 }
