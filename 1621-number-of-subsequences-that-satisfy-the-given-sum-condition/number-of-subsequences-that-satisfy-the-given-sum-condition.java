b90b783fa38ad9f1375bf61e39320938111fac1c
Shivamani R <158829688+Shivamani-R@users.noreply.github.com>
2025-06-29 08:10:59 +0530

    Time: 27 ms (99.52%) | Memory: 55.5 MB (32.50%) - LeetSync

--- 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.java
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.java
@@ -1,23 +1,26 @@
 class Solution {
     public int numSubseq(int[] nums, int target) {
-        int mod = 1000000007 , n = nums.length;
         Arrays.sort(nums);
-        
-        int[] power = new int[n];
-        power[0] = 1;
-        for (int i = 1; i < n; i++) {
-            power[i] = (power[i - 1] * 2) % mod;
+        int mod = 1000000007;
+        int n =nums.length;
+        int left=0;
+        int right = n-1;
+        int[] pw = new int[n];
+        pw[0] = 1;
+        for(int i=1  ; i<n ; i++){
+            pw[i] = (pw[i-1]*2)%mod;
         }
-        int left = 0 , right = n - 1 , result = 0;
-
-        while (left <= right) {
-            if (nums[left] + nums[right] <= target) {
-                result = (result + power[right - left]) % mod;
+        int ans=0;
+        while(left<=right){
+            if(nums[left]+nums[right]<=target){
+                ans=(ans+pw[right-left])%mod;
                 left++;
-            } else {
-                right--;
             }
+            else
+                right--;
+
         }
-        return result;
+        return ans;
     }
+    
 }
