d25dbfd315356ea17da424c09b298ab32de7ad58
Shivamani R <158829688+Shivamani-R@users.noreply.github.com>
2025-07-07 17:19:41 +0530

    Time: 64 ms (89.04%) | Memory: 75.3 MB (86.83%) - LeetSync

--- 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.java
+++ 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.java
@@ -1,26 +1,30 @@
 class Solution {
     public int maxEvents(int[][] events) {
-        Arrays.sort(events, (a, b) -> Integer.compare(a[0], b[0]));
-        
-        int day = 0, index = 0 , n = events.length ,result = 0;      
-        
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        while (!pq.isEmpty() || index < n) {
-            if (pq.isEmpty()) {
-                day = events[index][0];
-            }
-            while (index < n && events[index][0] <= day) {
-                pq.offer(events[index][1]);
-                index++;
-            }
-            pq.poll();
-            result++; 
-            day++;    
+        Arrays.sort(events, (a, b) -> a[0] - b[0]);
 
-            while (!pq.isEmpty() && pq.peek() < day) {
-                pq.poll();
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        int day = 1, i = 0, n = events.length, attended = 0;
+
+        while (i < n || !minHeap.isEmpty()) {
+            while (i < n && events[i][0] == day) {
+                minHeap.add(events[i][1]);
+                i++;
             }
+
+            while (!minHeap.isEmpty() && minHeap.peek() < day) {
+                minHeap.poll();
+            }
+
+            if (!minHeap.isEmpty()) {
+                minHeap.poll();
+                attended++;
+            }
+
+            day++;
         }
-        return result;
+
+        return attended;
     }
+
 }
+
