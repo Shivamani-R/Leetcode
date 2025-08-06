90a60c8e33b58f15ac05fac5d18a6c711744f378
Shivamani R <158829688+Shivamani-R@users.noreply.github.com>
2025-08-06 23:54:51 +0530

    Time: 9 ms (96.05%) | Memory: 57.1 MB (29.11%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -4,7 +4,6 @@ public class Solution {
     if (head == null || head.next == null)
       return head;
         
-    // step 1. cut the list to two halves
     ListNode prev = null, slow = head, fast = head;
     
     while (fast != null && fast.next != null) {
@@ -15,11 +14,10 @@ public class Solution {
     
     prev.next = null;
     
-    // step 2. sort each half
     ListNode l1 = sortList(head);
     ListNode l2 = sortList(slow);
     
-    // step 3. merge l1 and l2
+
     return merge(l1, l2);
   }
   
