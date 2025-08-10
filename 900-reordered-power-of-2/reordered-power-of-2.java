5575f564849e4de948de3fb11d1a507b1b87640f
Shivamani R <158829688+Shivamani-R@users.noreply.github.com>
2025-08-10 07:34:16 +0530

    Time: 1 ms (85.79%) | Memory: 41.4 MB (18.42%) - LeetSync

--- 900-reordered-power-of-2/reordered-power-of-2.java
+++ 900-reordered-power-of-2/reordered-power-of-2.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean reorderedPowerOf2(int n) {
         String sortedN = sortDigits(n);
         
-        for (int i = 0; i < 31; i++) { // 2^0 to 2^30 fits in int
+        for (int i = 0; i < 31; i++) { 
             int power = 1 << i;
             if (sortedN.equals(sortDigits(power))) {
                 return true;
