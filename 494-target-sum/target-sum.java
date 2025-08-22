beac8c09ad17b5cbd072d8b0c3c7282c87c24470
Shivamani R <158829688+Shivamani-R@users.noreply.github.com>
2025-08-22 18:16:31 +0530

    Time: 5 ms (83.74%) | Memory: 44.1 MB (39.58%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,27 +1,34 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        int sum = 0;
-        for (int x : nums) sum += x;
+       
+       int sum=0;
+       for(int x:nums){
+        sum += x;
+       }
+        if (sum < Math.abs(target) || (sum + target) % 2 != 0) {
+            return 0;
+        }
 
-        if (sum < Math.abs(target) || ((sum + target) & 1) == 1) return 0;
+       int s1 = (sum + target)/2;
 
-        int s1 = (sum + target) / 2;
-        return subsetSumCount(nums, s1);
+       return subsetsum(nums,s1);
     }
 
-    private int subsetSumCount(int[] nums, int s1) {
+    private int subsetsum(int[] nums,int s1){
         int n = nums.length;
-        int[][] dp = new int[n + 1][s1 + 1];
+        int[][] dp = new int[n+1][s1+1];
 
-        
-        for (int i = 0; i <= n; i++) dp[i][0] = 1;
+        for(int i=0;i<=n;i++){
+            dp[i][0] = 1;
+        }
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j <= s1; j++) { 
-                if (nums[i - 1] <= j) {
-                    dp[i][j] = dp[i - 1][j] + dp[i - 1][j - nums[i - 1]];
-                } else {
-                    dp[i][j] = dp[i - 1][j];
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<=s1;j++){
+                if(nums[i-1]<=j){
+                    dp[i][j] = dp[i-1][j] + dp[i-1][j-nums[i-1]];
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
                 }
             }
         }
