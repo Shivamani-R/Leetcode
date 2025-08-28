43e1988800c76f3b596ba2d882c4b8e564796782
Shivamani R <158829688+Shivamani-R@users.noreply.github.com>
2025-08-28 19:51:46 +0530

    Time: 2359 ms (41.99%) | Memory: 74.6 MB (5.15%) - LeetSync

---
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
@@ -0,0 +1,38 @@
+class Solution {
+    static int[][] dp;
+    public int minCut(String s) {
+        int n = s.length();
+        dp = new int[n+1][n+1];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+
+        return solve(s,0,n-1);
+    }
+
+   private static int solve(String s, int i, int j) {
+        if (i >= j || isPalindrome(s, i, j)) return 0;
+
+        if (dp[i][j] != -1) return dp[i][j];
+
+        int min = Integer.MAX_VALUE;
+
+        for (int k = i; k < j; k++) {
+            if (isPalindrome(s, i, k)) { 
+                int cost = 1 + solve(s, k + 1, j);
+                min = Math.min(min, cost);
+            }
+        }
+
+        return dp[i][j] = min;
+    }
+
+    private static boolean isPalindrome(String s, int i, int j) {
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
