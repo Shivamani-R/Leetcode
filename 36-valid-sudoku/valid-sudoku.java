2bf02db1888f0ad95da5a2eeda600db590caeb34
Shivamani R <158829688+Shivamani-R@users.noreply.github.com>
2025-08-30 12:43:19 +0530

    Time: 2 ms (77.42%) | Memory: 44.5 MB (56.61%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,40 +1,41 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        for (int row = 0; row < 9; row++) {
-            for (int col = 0; col < 9; col++) {
-                if (board[row][col] != '.') {
-                    if (!isSafe(board, row, col, board[row][col])) {
-                        return false;
-                    }
+        int N = 9;
+
+        HashSet<Character>[] rows = new HashSet[N];
+        HashSet<Character>[] cols = new HashSet[N];
+        HashSet<Character>[] boxes = new HashSet[N];
+        for (int r = 0; r < N; r++) {
+            rows[r] = new HashSet<Character>();
+            cols[r] = new HashSet<Character>();
+            boxes[r] = new HashSet<Character>();
+        }
+
+        for (int r = 0; r < N; r++) {
+            for (int c = 0; c < N; c++) {
+                char val = board[r][c];
+
+                if (val == '.') {
+                    continue;
                 }
-            }
-        }
-        return true;
-    }
 
-    public static boolean isSafe(char[][] sudoku, int row, int col, char digit) {
-        for (int i = 0; i < 9; i++) {
-            if (i != col && sudoku[row][i] == digit) {
-                return false;
-            }
-        }
-
-        for (int i = 0; i < 9; i++) {
-            if (i != row && sudoku[i][col] == digit) {
-                return false;
-            } 
-        }
-
-        int subgridStartRow = (row / 3) * 3;
-        int subgridStartCol = (col / 3) * 3;
-        for (int i = subgridStartRow; i < subgridStartRow + 3; i++) {
-            for (int j = subgridStartCol; j < subgridStartCol + 3; j++) {
-                if ((i != row || j != col) && sudoku[i][j] == digit) {
+                if (rows[r].contains(val)) {
                     return false;
-                } 
+                }
+                rows[r].add(val);
+
+                if (cols[c].contains(val)) {
+                    return false;
+                }
+                cols[c].add(val);
+
+                int idx = (r / 3) * 3 + c / 3;
+                if (boxes[idx].contains(val)) {
+                    return false;
+                }
+                boxes[idx].add(val);
             }
         }
-
         return true;
     }
 }
