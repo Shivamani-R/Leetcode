76bf0535939752f886d914df175ab02f920bea72
Shivamani R <158829688+Shivamani-R@users.noreply.github.com>
2025-09-22 12:47:02 +0530

    Time: 1 ms (99.77%) | Memory: 53.2 MB (39.21%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,21 @@
+public class Solution{
+    public int  majorityElement(int[] nums) {
+      int can = 0;
+      int count = 0;
+
+      for(int num:nums){
+        if(count == 0){
+            can = num;
+        }
+
+        if(num==can){
+            count++;
+        }
+        else{
+            count--;
+        }
+      }
+      return can;
+    }
+}
+      
