52192b90f47eda84a2bff4970d37a2511a068bb3
Shivamani R <158829688+Shivamani-R@users.noreply.github.com>
2025-10-27 16:45:21 +0530

    Time: 2 ms (16.65%) | Memory: 40.7 MB (38.23%) - LeetSync

---
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int totalMoney(int n) {
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            int week = i / 7;
+            int day = i % 7;
+            sum += week+day+1;
+        }
+        return sum;
+    }
+}
