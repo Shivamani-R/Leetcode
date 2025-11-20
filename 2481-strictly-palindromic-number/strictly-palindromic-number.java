1b14e7cddd388ffa40bf798232f8a69bd8ab40c0
Shivamani R <158829688+Shivamani-R@users.noreply.github.com>
2025-11-20 14:12:04 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (16.90%) - LeetSync

--- 2481-strictly-palindromic-number/strictly-palindromic-number.java
+++ 2481-strictly-palindromic-number/strictly-palindromic-number.java
@@ -1,5 +1,31 @@
 class Solution {
     public boolean isStrictlyPalindromic(int n) {
-        return false;
+        for (int base = 2; base <= n - 2; base++) {
+            String converted = convertToBase(n, base);
+            if (!isPalindrome(converted)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private String convertToBase(int n, int base) {
+        StringBuilder sb = new StringBuilder();
+        while (n > 0) {
+            sb.append(n % base);
+            n /= base;
+        }
+        return sb.reverse().toString();
+    }
+
+    private boolean isPalindrome(String s) {
+        int i = 0, j = s.length() - 1;
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
     }
 }
