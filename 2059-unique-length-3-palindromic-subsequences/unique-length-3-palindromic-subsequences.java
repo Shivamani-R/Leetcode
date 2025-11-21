f7a27cc97d4e83cd71c1413162f188467deacc1f
Shivamani R <158829688+Shivamani-R@users.noreply.github.com>
2025-11-21 10:39:39 +0530

    Time: 171 ms (79.63%) | Memory: 47.1 MB (17.63%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.java
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.java
@@ -1,36 +1,30 @@
 class Solution {
     public int countPalindromicSubsequence(String s) {
-        int n = s.length();
-        int[] first = new int[26];
-        int[] last = new int[26];
+       int n = s.length();
+       int[] first = new int[26];
+       int[] last = new int[26];
 
-        for (int i = 0; i < 26; i++) {
+       for (int i = 0; i < 26; i++) {
             first[i] = Integer.MAX_VALUE;
             last[i] = Integer.MIN_VALUE;
         }
 
+       for(int i=0;i<n;i++){
+        int c = s.charAt(i)-'a';
+        first[c]=Math.min(first[c],i);
+        last[c]=Math.max(last[c],i);
+       }
+        int res=0;
+        for(int i=0;i<26;i++){
+        if(first[i]<last[i]){
+            HashSet<Character> x = new HashSet<>();
 
-        for (int i = 0; i < n; i++) {
-            int c = s.charAt(i) - 'a';
-            first[c] = Math.min(first[c], i);
-            last[c] = Math.max(last[c], i);
-        }
-
-        int res = 0;
-
-        for (int i = 0; i < 26; i++) {
-            if (first[i] < last[i]) {
-
-                HashSet<Character> set = new HashSet<>();
-
-                for (int j = first[i] + 1; j < last[i]; j++) {
-                    set.add(s.charAt(j));
-                }
-
-                res += set.size();
+            for(int j=first[i]+1;j<last[i];j++){
+                x.add(s.charAt(j));
             }
+            res+=x.size();
         }
-
-        return res;
+       }
+    return res;
     }
 }
