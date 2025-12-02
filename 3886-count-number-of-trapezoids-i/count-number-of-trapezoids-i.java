056111af6c5779d1fd749e95e346418e7877ca9b
Shivamani R <158829688+Shivamani-R@users.noreply.github.com>
2025-12-02 19:21:08 +0530

    Time: 35 ms (89.29%) | Memory: 207.2 MB (13.78%) - LeetSync

--- 3886-count-number-of-trapezoids-i/count-number-of-trapezoids-i.java
+++ 3886-count-number-of-trapezoids-i/count-number-of-trapezoids-i.java
@@ -1,18 +1,15 @@
 class Solution {
     public int countTrapezoids(int[][] points) {
-        long MOD = 1_000_000_007L;
-        java.util.HashMap<Integer, Long> map = new java.util.HashMap<>();
-        for (int[] p : points) {
-            map.put(p[1], map.getOrDefault(p[1], 0L) + 1);
+        Map<Integer, Integer> pointNum = new HashMap<>();
+        final int mod = 1000000007;
+        long ans = 0, sum = 0;
+        for (int[] point : points) {
+            pointNum.put(point[1], pointNum.getOrDefault(point[1], 0) + 1);
         }
-        java.util.ArrayList<Long> seg = new java.util.ArrayList<>();
-        for (long k : map.values()) {
-            if (k >= 2) seg.add((k * (k - 1) / 2) % MOD);
-        }
-        long sum = 0, ans = 0;
-        for (long v : seg) {
-            ans = (ans + v * sum) % MOD;
-            sum = (sum + v) % MOD;
+        for (int pNum : pointNum.values()) {
+            long edge = ((long) pNum * (pNum - 1)) / 2;
+            ans = (ans + edge * sum) % mod;
+            sum = (sum + edge) % mod;
         }
         return (int) ans;
     }
